16aa0b6cde8b3223b5299d605628b58ba4836f15
Apheiro <eyentuyon@gmail.com>
2023-03-27 21:40:03 -0300

    Make random seleccion card fn.

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -3,72 +3,103 @@ import StartMenu from '../components/StartMenu/StartMenu';
 import DifficultyMenu from '../components/DifficultyMenu/DifficultyMenu';
 import Game from '../components/Game/Game';
 import { useState, useEffect } from 'react'
+import useStateCC from '../hooks/useStateCC';
 
 interface Character {
   id: number,
   image: string,
-  name: string
+  name: string,
+  picked: boolean
 }
 
 function App() {
   const [startGame, setStartGame] = useState<boolean>(false);
-  const [difficulty, setDifficulty] = useState<string>('d0');
+  const [difficulty, setDifficulty] = useState<number>(0);
   const [showGame, setShowGame] = useState<boolean>(false);
-  const [characters, setCharacters] = useState<Character[]>([]);
+  const [characters, setCharacters] = useStateCC<Character[]>([]);
 
-  function startGameFn(): void {
-    setStartGame(true);
+  function getUrls(max?: number, excludeID: number[] = []): string[] {
+    const maxNum: number = max !== undefined ? max : 0;
+    let charactersUrl: string[] = [];
+    for (let i: number = 0; i < maxNum; i++) {
+      let randomID: number;
+      do { randomID = Math.round(Math.random() * 731); }
+      while (charactersUrl.includes(`https://www.superheroapi.com/api.php/1614328315682782/${randomID}`) || excludeID.includes(randomID))
+      charactersUrl.push(`https://www.superheroapi.com/api.php/1614328315682782/${randomID}`);
+    }
+    return charactersUrl
   }
 
-  function getDifficultyFn(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-    setDifficulty(e.currentTarget.id);
-  }
+  function fillCharacters(max: number, excludedID: number[] = [], allCharactersSaved: Character[] = []): void {
+    const Urls: string[] = getUrls(max, excludedID);
+    const allCharacters = Promise.all(Urls.map(async (url: string) => {
+      const characterInfoRequest: Response = await fetch(url);
+      const characterInfoJson: any = await characterInfoRequest.json();
+      return {
+        id: parseInt(characterInfoJson.id),
+        image: characterInfoJson.image.url,
+        name: characterInfoJson.name,
+        picked: false
+      } as Character;
+    }));
 
-  async function fillCharacters(max: number): Promise<void> {
-    try {
-      for (let i: number = 0; i < max; i++) {
-        const randomID: number = Math.round(Math.random() * 731);
-        const characterInfoRequest: Response = await fetch(`https://www.superheroapi.com/api.php/1614328315682782/${randomID}`);
-        const characterInfoJson: any = await characterInfoRequest.json()
-        const character: Character = {
-          id: parseInt(characterInfoJson.id),
-          image: characterInfoJson.image.url,
-          name: characterInfoJson.name
+    allCharacters.then(characters => {
+      const promises = characters.map((char: Character) => {
+        return new Promise<Character>((resolve, reject) => {
+          const img = new Image();
+          img.src = char.image;
+          img.onload = () => resolve(char)
+          img.onerror = () => reject(char)
+        })
+      })
+      const allImgLoadPromise = Promise.allSettled(promises)
+
+      allImgLoadPromise.then(charPromResults => {
+        let allCharNoImg = charPromResults
+          .filter((char): char is PromiseRejectedResult => char.status === 'rejected')
+          .map((chars) => chars.reason as Character)
+        if (allCharNoImg.length !== 0) {
+          const maxNumberOfNewCharacters: number = allCharNoImg.length;
+          const excludedID: number[] = allCharNoImg.map(char => char.id);
+          allCharNoImg.forEach((char) => {
+            const charIndex: number = characters.findIndex(c => c === char);
+            characters.splice(charIndex, 1);
+          })
+          const allCharacters = [...allCharactersSaved, ...characters]
+          fillCharacters(maxNumberOfNewCharacters, excludedID, allCharacters);
+        } else {
+          const allCharacters = [...allCharactersSaved, ...characters];
+          setCharacters(allCharacters, () => {
+            setShowGame(true);
+          });
         }
-        setCharacters((prevState) => [...prevState, character])
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
+      })
 
-  function getCharacters(): void {
-    if (difficulty === 'd1') {
-      fillCharacters(10).then(() => setShowGame(true));
-    } else if (difficulty === 'd2') {
-      fillCharacters(20).then(() => setShowGame(true));
-    } else if (difficulty === 'd3') {
-      fillCharacters(30).then(() => setShowGame(true));
-    }
+    });
+
   }
 
   useEffect(() => {
-    console.log(difficulty);
-    getCharacters();
+    if (difficulty !== 0) fillCharacters(difficulty)
   }, [difficulty])
-  useEffect(() => {
-    console.log(characters);
-  }, [characters])
+
 
   return (
     <div className="App">
+      <button onClick={() => {
+        console.log('startgame', startGame)
+        console.log('showGame', showGame)
+        console.log('characters', characters)
+      }}>Test</button>
       {
         showGame ?
           <Game characters={characters} /> :
           startGame ?
-            <DifficultyMenu getDifficultyFn={getDifficultyFn} /> :
-            <StartMenu startGameFn={startGameFn} />
+            <DifficultyMenu setDifficulty={setDifficulty} /> :
+            <StartMenu setStartGame={setStartGame} />
       }
+
+      <a href='https://github.com/Apheiro/memory_card'>G</a>
     </div>
   );
 }
